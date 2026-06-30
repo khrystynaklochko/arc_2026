01f99ced0d6a864dee58f96d278ec0e0d8386050
Khrystyna Klochko <khrystynaklochko@gmail.com>
2026-06-30 23:40:42 +0200

    Export FuzzyRecursiveAgent from agents module

--- agents/__init__.py
+++ agents/__init__.py
@@ -5,7 +5,8 @@ ARC-AGI-3 Agent implementations
 from .base_agent import BaseAgent
 from .random_agent import RandomAgent
 from .llm_agent import LLMAgent, OpenAIAgent, AnthropicAgent
+from .fuzzy_agent import FuzzyRecursiveAgent
 
-__all__ = ['BaseAgent', 'RandomAgent', 'LLMAgent', 'OpenAIAgent', 'AnthropicAgent']
+__all__ = ['BaseAgent', 'RandomAgent', 'LLMAgent', 'OpenAIAgent', 'AnthropicAgent', 'FuzzyRecursiveAgent']
 
 # Made with Bob
